782df6318cf3806c7ca2cb86f758af0a7b8d8e93
irubido <ivan.rubido@gmail.com>
2023-05-12 16:05:28 +0200

    aleo_getAccount

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,7 +1,8 @@
 import { OnRpcRequestHandler } from "@metamask/snaps-types";
 import { getAccount } from "./rpc/getAccount";
+import { InitOutput, initializeWasm } from "aleo-snap-wasm";
 
- enum Methods {
+enum Methods {
   GetAccount = "aleo_getAccount",
   GetViewKey = "aleo_getViewKey",
   Decrypt = "aleo_decrypt",
@@ -9,11 +10,20 @@ import { getAccount } from "./rpc/getAccount";
   Verify = "aleo_verify",
 }
 
+let wasm: InitOutput;
+
 export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
-  
+
+  if (!wasm) {
+    wasm = await initializeWasm();
+  } else {
+    console.log("initialized wasm");
+  }
+
   switch (request.method) {
-    case Methods.GetAccount:
+    case Methods.GetAccount: {
       return getAccount();
+    }
     default:
       throw new Error("Method not found.");
   }
--- packages/snap/src/rpc/getAccount.ts
+++ packages/snap/src/rpc/getAccount.ts
@@ -1,44 +1,7 @@
-// const fs = require('fs');
-// let wasm: WebAssembly.WebAssemblyInstantiatedSource;
+import { Address } from "aleo-snap-wasm";
+import { getPrivateKey } from "../aleo/account";
 
 export const getAccount = async () => {
-  const node = (await snap.request({
-    method: `snap_getBip44Entropy`,
-    params: {
-      coinType: 1,
-    },
-  }))
-  console.log(node)
-
-  // const wasmBuffer = fs.readFileSync(`${__dirname}/../dist/aleo_bg.wasm`);
-  // console.log(wasmBuffer);
-  // wasm = await WebAssembly.instantiate(wasmBuffer);
-  // console.log(wasm);
-
-  // await fetch("https://cdn.jsdelivr.net/npm/@aleohq/wasm@0.4.1/aleo_bg.wasm")
-  //   .then((response) => {
-  //     console.log("response");
-  //     console.log(response);
-  //     return response.arrayBuffer()
-  //   })
-  //   .then((bytes) => {
-  //     console.log("bytes");
-  //     console.log(bytes);
-  //     return WebAssembly.instantiate(bytes, {})
-  //   })
-  //   .then((results) => {
-  //     console.log("results")
-  //     console.log(results)
-  //   }).catch(console.error);
-  // console.log("after wasm");
-
-  // await WebAssembly.instantiateStreaming(fetch("https://cdn.jsdelivr.net/npm/@aleohq/wasm@0.4.1/aleo_bg.wasm"), {}).then(
-  //   (results) => {
-  //     console.log("results")
-  //     console.log(results)
-  //   }
-  // ).catch(console.error);
-  //   console.log("after wasm")
-
-  return 2;
+  const privateKey = await getPrivateKey();
+  return Address.from_private_key(privateKey).to_string();
 }
