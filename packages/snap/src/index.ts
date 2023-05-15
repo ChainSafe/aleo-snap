4d0cbf5681021aa59b01fbd2d2738758cb63eb1a
BeroBurny <bero4net@gmail.com>
2023-05-15 11:22:09 +0200

    revert?

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,49 +1,15 @@
 import { OnRpcRequestHandler } from "@metamask/snaps-types";
 
-// @ts-ignore
-import wasmInit, { InitOutput, PrivateKey } from "aleo-snap-wasm";
-
-let wasm: InitOutput;
-
-const initializeWasm = async (): Promise<void> => {
-  console.log("initializeWasm v4");
-  try {
-    const wasmFile = await fetch(
-      "http://localhost:8081/build/aleo_wasm_bg.wasm"
-    ).then((r) => r.arrayBuffer());
-    console.warn(wasmFile);
-    const wmodule = WebAssembly.compile(wasmFile);
-    console.log(wmodule);
-    wasm = await wasmInit(wmodule);
-
-    console.log(PrivateKey.from_string("asasasasas"));
-    console.log(wasm);
-  } catch (error) {
-    console.error("Failed to initialize WebAssembly module.", error);
-    throw error;
-  }
-};
-
-enum Methods {
-  GetAccount = "aleo_getAccount",
-  GetViewKey = "aleo_getViewKey",
-  Decrypt = "aleo_decrypt",
-  Sign = "aleo_sign",
-  Verify = "aleo_verify",
+export enum Methods {
+  GetAddress = "aleo_getAddress",
 }
 
 export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
-  if (!wasm) {
-    await initializeWasm();
-  } else {
-    console.log("initialized wasm");
-  }
-
   switch (request.method) {
-    case "ping": {
-      console.warn(wasm);
-      return "pong";
-    }
+    case Methods.GetAddress:
+      return await new Promise((resolve) => {
+        resolve(null);
+      });
     default:
       throw new Error("Method not found.");
   }
