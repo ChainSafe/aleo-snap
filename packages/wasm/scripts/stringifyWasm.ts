f88a71d38512d352e97fc11f283a7f599f581d4a
BeroBurny <bero4net@gmail.com>
2023-05-15 11:17:46 +0200

    improve wasm script v2

--- packages/wasm/scripts/stringifyWasm.ts
+++ packages/wasm/scripts/stringifyWasm.ts
@@ -1,8 +1,7 @@
 import { readFileSync, writeFileSync } from "node:fs";
 import { resolve } from "node:path";
 
-// require.resolve('module');
-const wasmPath = "src/wasmBuild/aleo_wasm_bg.wasm";
+const wasmPath = require.resolve('@aleohq/wasm/aleo_bg.wasm');
 
 const createFileString = (array: string): string =>
 `// Pre generated file please do not edit
@@ -10,6 +9,12 @@ export const wasm = "${array}";
 `;
 
 (async function() {
-  const wasmBuffer = readFileSync(resolve(wasmPath), { encoding: "base64" });
-  writeFileSync(resolve("src/wasm.ts"), createFileString(wasmBuffer));
+  console.log("Stringify wasm file from:", wasmPath);
+  const wasmBuffer = readFileSync(wasmPath, { encoding: "base64" });
+
+  const destination = resolve("src/wasm.ts");
+  console.log("Storing file to location:", destination);
+  writeFileSync(destination, createFileString(wasmBuffer));
+
+  console.log("\nFile stored 👌");
 })();
