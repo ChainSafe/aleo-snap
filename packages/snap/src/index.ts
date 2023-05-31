e5f5c508f9626cb7e0852b4dc5200dbd72032775
Bernard Stojanović <bero4net@gmail.com>
2023-05-31 16:25:44 +0200

    fix: view key and tests (#10)
    * implement tests for getPrivateKey
    
    * implement prompt for accessing view key
    
    * add snaps-ui and chai-as-promised lib
    
    * throw error on rejecting getViewKey
    
    * implement e2e
    
    * fix e2e

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -3,7 +3,7 @@ import { InitOutput, initializeWasm } from "@chainsafe/aleo-snap-wasm";
 import { getAccount } from "./rpc/getAccount";
 import { getViewKey } from "./rpc/getViewKey";
 
-enum Methods {
+export enum Methods {
   GetAccount = "aleo_getAccount",
   GetViewKey = "aleo_getViewKey",
   Decrypt = "aleo_decrypt",
@@ -13,7 +13,10 @@ enum Methods {
 
 let wasm: InitOutput;
 
-export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
+export const onRpcRequest: OnRpcRequestHandler = async ({
+  request,
+  origin,
+}) => {
   if (!wasm) {
     wasm = await initializeWasm();
   }
@@ -23,7 +26,7 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
       return getAccount(snap);
     }
     case Methods.GetViewKey: {
-      return getViewKey(snap);
+      return getViewKey(snap, origin);
     }
     default:
       throw new Error("Method not found.");
--- packages/snap/test/unit/rpc/getViewKey.test.ts
+++ packages/snap/test/unit/rpc/getViewKey.test.ts
@@ -1,12 +1,14 @@
-import chai, { expect } from "chai";
+import { expect, use } from "chai";
 import sinon from "sinon";
 import sinonChai from "sinon-chai";
-import { mockSnapProvider } from "./wallet.stub";
-import { bip44Entropy1Node } from "../aleo/bip44Entropy.mock";
+import chaiAsPromised from "chai-as-promised";
 import { initializeWasm } from "@chainsafe/aleo-snap-wasm";
+import { bip44Entropy1Node } from "../aleo/bip44Entropy.mock";
 import { getViewKey } from "../../../src/rpc/getViewKey";
+import { mockSnapProvider } from "./wallet.stub";
 
-chai.use(sinonChai);
+use(sinonChai);
+use(chaiAsPromised);
 
 describe("Test rpc handler function: getViewKey", function () {
   const sanbox = sinon.createSandbox();
@@ -20,14 +22,31 @@ describe("Test rpc handler function: getViewKey", function () {
     sanbox.reset();
   });
 
-  it("should return aleo viewKey", async function () {
+  it("should return aleo viewKey after positive confirmation", async function () {
     snapStub.request
       .withArgs(sinon.match.has("method", "snap_getBip44Entropy"))
       .resolves(bip44Entropy1Node);
+    snapStub.request
+      .withArgs(sinon.match.has("method", "snap_dialog"))
+      .resolves(true);
 
-    const account = await getViewKey(snapStub);
+    const account = await getViewKey(snapStub, "https://fake.com");
 
-    expect(account).to.be.eql("AViewKey1icf4Y5hujfpeLBpxfMSX2V8UGNVJ9pUa4m5SQd1HkWy5");
+    expect(account).to.be.eql(
+      "AViewKey1icf4Y5hujfpeLBpxfMSX2V8UGNVJ9pUa4m5SQd1HkWy5"
+    );
   });
 
+  it("should not return aleo viewKey after negative confirmation", async function () {
+    snapStub.request
+      .withArgs(sinon.match.has("method", "snap_getBip44Entropy"))
+      .resolves(bip44Entropy1Node);
+    snapStub.request
+      .withArgs(sinon.match.has("method", "snap_dialog"))
+      .resolves(false);
+
+    await expect(getViewKey(snapStub, "https://fake.com")).to.be.rejectedWith(
+      "User decline request"
+    );
+  });
 });
