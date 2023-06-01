bc475d8b131db38e41b391cbe195498b5bea38fc
irubido <ivan.rubido@gmail.com>
2023-06-01 13:27:25 +0200

    add test coverage

--- packages/snap/test/unit/rpc/decryptRecord.test.ts
+++ packages/snap/test/unit/rpc/decryptRecord.test.ts
@@ -5,6 +5,7 @@ import { initializeWasm } from "@chainsafe/aleo-snap-wasm";
 import { decryptRecord } from "../../../src/rpc/decryptRecord";
 import { mockSnapProvider } from "./wallet.stub";
 import { bip44Entropy1Node } from "../aleo/bip44Entropy.mock";
+import { getAccount } from "../../../src/rpc/getAccount";
 
 chai.use(sinonChai);
 
@@ -47,5 +48,26 @@ describe("Test rpc handler function: decryptRecord", function () {
         cipherText: "record1qyqsqpe2szk2wwwq56akkwx586hkndl3r8vzdwve32lm7elvphh37rsyqyxx66trwfhkxun9v35hguerqqpqzqrtjzeu6vah9x2me2exkgege824sd8x2379scspmrmtvczs0d93qttl7y92ga0k0rsexu409hu3vlehe3yxjhmey3frh2z5pxm5cmxsv4un97q",
       }
     )).to.be.rejectedWith("Decryption failed - view key did not match record");
+  });
+
+  it("should return decrypted record only with cipherthext if encrypted by owner", async function () {
+    snapStub.request
+    .withArgs(sinon.match.has("method", "snap_getBip44Entropy"))
+    .resolves(bip44Entropy1Node);
+
+    const record = await decryptRecord(
+      snapStub,
+      {
+        cipherText: "record1qyqspcvr3q75x5klxaa88ysjcpx36gz0zalsx7hjn5mtttqndcffxmgzqyxx66trwfhkxun9v35hguerqqpqzq83z5d5jpqwwhdglt05lnaq83dnnkfukg2upfwzywx849s5mkklqssure3s6926z2us892c84dqjm8qarn64z0pcqqw2l3t29jen4cs6c0qfnm",
+      }
+    );
+
+    const account = await getAccount(snapStub);
+
+    expect(account).to.be.eql("aleo1j2mxy4kp3snsrzhs95dlar6eyekerg62h4yetw93eufw76u8qq9q7ar5qz");
+
+    expect(record.substring(11, account.length + 11)).to.be.eql(account);
+
+    expect(record).to.be.eql("{\n  owner: aleo1j2mxy4kp3snsrzhs95dlar6eyekerg62h4yetw93eufw76u8qq9q7ar5qz.private,\n  microcredits: 50000000u64.private,\n  _nonce: 6080806725905813185393881582373108458266388201126403542594037621192938406177group.public\n}");
   });
 });
